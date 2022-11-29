7947df03f538d88a314315ed007e08c89c24f7f7
Victor <victorreis222@gmail.com>
2022-11-29 18:24:26 -0300

    Requisito 08 completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ProfilePicture from '../images/profileIcon.svg'
 import SearchIcon from '../images/searchIcon.svg'
+import { useHistory } from "react-router-dom";
 
 function Header({ title }) {
+
+  let history = useHistory();
+  const handleClick = () => {
+    history.push("/profile");
+  }
+
   return (
     <header className='header-div'>
       <h1 data-testid='page-title'>{title}</h1>
-      <img src={ProfilePicture} alt='imagem de perfil' data-testid='profile-top-btn' />
+      <img src={ProfilePicture} alt='imagem de perfil' data-testid='profile-top-btn' onClick={handleClick} />
       {title === 'Meals' || title === 'Drinks' ? <img src={SearchIcon} alt='ícone de pesquisa' data-testid='search-top-btn' /> : ''}
     </header>
   );
