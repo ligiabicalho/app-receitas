3a8efd8f242f75e212e78afffea4eb061edc1813
Victor <victorreis222@gmail.com>
2023-01-08 18:52:52 -0300

    Req 55 e 56 sem testes

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -4,9 +4,10 @@ import Header from '../components/Header';
 import blackHeart from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import '../styles/Favorites.css';
+import { Link } from 'react-router-dom';
 
 function FavoriteRecipes() {
-/*   const [filter, setFilter] = useState('all'); */
+  const [filter, setFilter] = useState('all');
   const [favorited, setFavorited] = useState([]);
   const [copied, setCopied] = useState('');
   useEffect(() => {
@@ -15,20 +16,64 @@ function FavoriteRecipes() {
       setFavorited(localFavorites);
     }
   }, []);
+
+  const filterMeal = () => {
+    const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const mealFiltered = localFavorites.filter((e) => e.type === 'meal');
+    setFavorited(mealFiltered);
+    setFilter('meal');
+  };
+
+  const filterDrinks = () => {
+    const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const drinkFiltered = localFavorites.filter((e) => e.type === 'drink');
+    setFavorited(drinkFiltered);
+    setFilter('drink');
+  };
+
+  const allRecipes = () => {
+    const localFavorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setFavorited(localFavorites);
+    setFilter('all');
+  };
+
   return (
     <div>
       <Header title="Favorite Recipes" />
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-meal-btn">Meals</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ allRecipes }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+        onClick={ filterMeal }
+      >
+        Meals
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ filterDrinks }
+      >
+        Drinks
+      </button>
       {favorited.map((e, index) => (
         <div key={ e.id } className="favorite-line">
-          <img
-            src={ e.image }
-            alt="Imagem da receita"
-            className="favorite-images"
-            data-testid={ `${index}-horizontal-image` }
-          />
+          <Link
+            to={ e.type === 'meal' ? `/meals/${e.id}`
+              : `/drinks/${e.id}` }
+          >
+            <img
+              src={ e.image }
+              alt="Imagem da receita"
+              className="favorite-images"
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </Link>
           <p
             className="text"
             data-testid={ `${index}-horizontal-top-text` }
@@ -39,13 +84,17 @@ function FavoriteRecipes() {
             {' '}
             { e.category }
           </p>
-          <p
-            className="text"
-            data-testid={ `${index}-horizontal-name` }
+          <Link
+            to={ e.type === 'meal' ? `/meals/${e.id}`
+              : `/drinks/${e.id}` }
           >
-            { e.name }
-
-          </p>
+            <p
+              className="text"
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { e.name }
+            </p>
+          </Link>
           <button type="button" className="icons">
             <img
               src={ blackHeart }
